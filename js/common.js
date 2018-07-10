731492386a6f5442d673c0029085a1d8b57724ae
Exception <samsonov199625@gmail.com>
2018-07-10 17:03:32 +0300

    first commit

---
+++ js/common.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+	
+$('.slider-offer').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    dots:true,
+    items:1,
+    navText:false,
+    smartSpeed:900
+
+    // responsive:{
+    //     1200: {
+    //     	items: 1
+    //     }
+    // }
+});
+
+$('.slider-order').owlCarousel({
+    loop:true,
+    margin:60,
+    nav:true,
+    dots:true,
+    items:3,
+    navText:false,
+    smartSpeed:900,
+    responsive:{
+        1200: {
+        	items: 3
+         }
+     }
+});
+
+
+$('.slider-souce').owlCarousel({
+    loop:true,
+    margin:60,
+    nav:true,
+    dots:true,
+    items:4,
+    navText:false,
+    smartSpeed:900,
+    responsive:{
+        1200: {
+            items: 4
+         }
+     }
+});
+
+$('.slider-drinks').owlCarousel({
+    loop:true,
+    margin:60,
+    nav:true,
+    dots:true,
+    items:3,
+    navText:false,
+    smartSpeed:900,
+    responsive:{
+        1200: {
+            items: 3
+         }
+     }
+});
+
+// ymaps.ready(function(){
+//      var myMap = new ymaps.Map('map', {
+//             center: [55.751574, 37.573856],
+//             zoom: 9
+//         })
+// });
+
+
+
+new WOW().init({
+    offset:1
+});
+
+
+});
